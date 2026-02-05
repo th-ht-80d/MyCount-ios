4732193dd31343092652f6f28358a48400cdc233
takahiro_hama <21331104th@tama.ac.jp>
2026-02-05 11:12:21 +0900

    Initial Commit

---
+++ MyCount/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCount
+//
+//  Created by 濱貴大 on 2026/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyCount/MyCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCountApp.swift
+//  MyCount
+//
+//  Created by 濱貴大 on 2026/02/05.
+//
+
+import SwiftUI
+
+@main
+struct MyCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
